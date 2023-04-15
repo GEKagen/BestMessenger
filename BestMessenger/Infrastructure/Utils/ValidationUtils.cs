3ae8a243d6bed990a7f485d3e43671f6e6841b34
GlukhovEv <08skywalker14@gmail.com>
2023-04-15 18:43:01 +0300

    Initial commit :q
    :wq

---
+++ BestMessenger/Infrastructure/Utils/ValidationUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestMessenger.Infrastructure.Utils
+{
+    public static class ValidationUtils
+    {
+
+        public static bool NameValidate(string value)
+        {
+            if (value.Length == 0)
+            {
+                return false;
+            }
+            foreach (var item in value)
+            {
+                if (Char.IsDigit(item) == true )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool EmailValidate(string value)
+        {
+            return true;
+        }
+        public static bool PasswordValidate(string value)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ BestMessenger/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,73 @@
+﻿using BestMessenger.Infrastructure.Commands;
+using BestMessenger.Infrastructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestMessenger.ViewModels
+{
+    public class RegistrationViewModel : BaseViewModel
+    {
+        #region Properties
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { UpdateValue<string>(ref firstName, value); }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { UpdateValue<string>(ref lastName, value); }
+        }
+
+        private string nickName;
+
+        public string NickName
+        {
+            get { return nickName; }
+            set { UpdateValue<string>(ref nickName, value);}
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { UpdateValue<string>(ref email, value); }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { UpdateValue<string>(ref password, value); }
+        }
+        #endregion
+
+        #region Commands
+        public ActionCommand RegistrationCommand => new ActionCommand(x => ValidateProperties());
+        #endregion
+
+        public RegistrationViewModel()
+        {
+            
+        }
+        private void ValidateProperties()
+        {
+            ValidationUtils.NameValidate(FirstName);
+            ValidationUtils.NameValidate(LastName);
+            ValidationUtils.NameValidate(NickName);
+            ValidationUtils.EmailValidate(Email);
+            ValidationUtils.PasswordValidate(Password);
+        }
+    }
+}
